d2debc2524646dba8bfad232217aad8a149d6d69
Joyce <joyceyukang@gmail.com>
2023-01-30 09:26:21 -0800

    one more try with edit in the dunder seeds file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,7 +19,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_messages
+        undo_messages()
         undo_join_table()
         undo_channels()
         undo_users()
@@ -33,7 +33,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_messages
+    undo_messages()
     undo_join_table()
     undo_channels()
     undo_users()
