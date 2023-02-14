76d4ba96adcfab2fb05b7b806a64117ad78674c2
Joyce <joyceyukang@gmail.com>
2023-02-13 22:10:18 -0800

    added some more css to splash page

--- react-app/src/components/SplashPage/Splash.js
+++ react-app/src/components/SplashPage/Splash.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Splash.css'
 import screen from './screen.png'
+import splash from './splash.png'
 
 const SplashPage = () => {
 
@@ -9,13 +10,15 @@ const SplashPage = () => {
             <div className="splash-backdrop">
                 <div className="splash-card-container">
                     <div className="splash-card">
-                        <h1>Welcome to slacker!</h1>
-                        <h4>Where slacking happens...</h4>
-                        <img src={screen} 
-                        alt="Girl in a jacket"
-                        width="550px"
-                        height="250px"
+                        <img src={splash} 
+                        alt="img"
+                        width="600px"
+                        height="400px"
                         ></img>
+                        <div>
+                        <h1>Welcome to slacker!</h1>
+                        <h4>Where chatting happens...</h4>
+                        </div>
                     </div>
                 </div>
             </div>
